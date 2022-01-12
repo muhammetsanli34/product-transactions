da9452ff71a023fe99cc28f497b0b2605e3dd3c9
sanlimamy <69751354+sanlimamy@users.noreply.github.com>
2022-01-12 15:43:25 +0300

    second-commit

--- src/store/actions.js
+++ src/store/actions.js
@@ -18,23 +18,3 @@ export const getTradeResult = ({commit}) => {
         commit("updateTradeResult",response.body);
     })
 }
-// import Vue from "vue";
-
-// export const setTradeResult = ({state, commit}, tradeResult) => {
-//   commit("updateTradeResult", tradeResult);
-//   let tradeData = {
-//     purchase: state.purchase,
-//     sale: state.sale
-//   }
-//   Vue.http.put("https://urun-islemleri-29797-default-rtdb.firebaseio.com/trade-result.json", tradeData)
-//     .then(response => {
-//     })
-// }
-// export const getTradeResult = ({commit}) => {
-
-//   Vue.http.get("https://urun-islemleri-29797-default-rtdb.firebaseio.com/trade-result.json")
-//     .then(response => {
-//       console.log(response);
-//       commit("updateTradeResult", response.body)
-//     })
-// }
